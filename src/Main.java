1a2c5381122039e7b3efac82560fb71eba93f4b4
gustavosouzaz <gustavodsouzaaa@gmail.com>
2024-03-27 14:29:37 -0300

    Instanciando e Teste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import onedigitalinnovation.SingletonEagle;
+import onedigitalinnovation.SingletonLazy;
+import onedigitalinnovation.SingletonLazyHolder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SingletonLazy lazy =  SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+
+        SingletonEagle eager =  SingletonEagle.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEagle.getInstancia();
+        System.out.println(eager);
+
+
+
+        SingletonLazyHolder lazyholder =  SingletonLazyHolder.getInstancia();
+        System.out.println(lazyholder);
+        lazyholder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyholder);
     }
 }
